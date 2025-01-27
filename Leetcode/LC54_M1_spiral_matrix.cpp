403dad5e85b109510e412021c6f927267f10db59
Harsh_Narayan <harshnarayannishad@gmail.com>
2025-01-27 08:11:11 +0530

    Spiral Method 2

--- Leetcode/LC54_M1_spiral_matrix.cpp
+++ Leetcode/LC54_M1_spiral_matrix.cpp
@@ -4,71 +4,50 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int> >& matrix) {
-        vector<int>res;
+    vector<int> spiralOrder(const vector<vector<int>>& matrix) {
+        vector<int> res;
         if (matrix.empty() || matrix[0].empty()) return res;
-
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int m = static_cast<int>(matrix.size());
+        int n = static_cast<int>(matrix[0].size());
         int top = 0, bottom = m - 1, left = 0, right = n - 1;
-
         while (top <= bottom && left <= right) {
             // Traverse from left to right
             for (int i = left; i <= right; ++i) res.push_back(matrix[top][i]);
             ++top;
-
             // Traverse from top to bottom
             for (int i = top; i <= bottom; ++i) res.push_back(matrix[i][right]);
             --right;
-
             // Traverse from right to left
             if (top <= bottom) {
                 for (int i = right; i >= left; --i) res.push_back(matrix[bottom][i]);
                 --bottom;
             }
-
             // Traverse from bottom to top
             if (left <= right) {
                 for (int i = bottom; i >= top; --i) res.push_back(matrix[i][left]);
                 ++left;
             }
         }
-
         return res;
     }
 };
 
 int main() {
-    // Dimension of matrix
-    int rows, cols;
-    cout << "Enter the number of rows: ";
-    cin >> rows;
-    cout << "Enter the number of columns: ";
-    cin >> cols;
-
-    // Create a 2D vector to store the matrix
-    vector<vector<int> > matrix(rows, vector<int>(cols));
-
-    // Input the elements of the matrix using a nested for loop
-    cout << "Enter the elements of the matrix row by row:" << endl;
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
-
+    // Define a 3x3 matrix using push_back
+    vector<vector<int>> matrix;
+    matrix.push_back(vector<int>{1, 2, 3});
+    matrix.push_back(vector<int>{4, 5, 6});
+    matrix.push_back(vector<int>{7, 8, 9});
     // Create an instance of the Solution class
     Solution solution;
-
     // Call the spiralOrder method
     vector<int> result = solution.spiralOrder(matrix);
-
     // Print the result
     cout << "Spiral order: ";
-    for (int num : result) {
-        cout << num << " ";
+    for (size_t i = 0; i < result.size(); ++i) {
+        cout << result[i];
+        if (i != result.size() - 1) cout << " ";
     }
     cout << endl;
-
     return 0;
 }
